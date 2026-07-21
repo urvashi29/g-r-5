56688f5718a7d34ddf97c4109280cbf3150836df
urvashi29 <urvashigolu29@gmail.com>
2026-07-21 13:56:30 +0530

    first commit

---
+++ session-1/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+// Function based component
+const App = () => {
+  return (
+    // JSX: xtensible markup language
+    <>
+      <h2>Hello</h2>
+      <h2>World</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga culpa
+        beatae voluptas natus, ipsam tenetur adipisci. Eligendi, inventore
+        accusantium distinctio repellat dolore quia tenetur dicta aliquid.
+        Explicabo quasi id consequatur!
+      </p>
+    </>
+  );
+};
+
+export default App;
---
+++ session-2/code/src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Try creating products display in UI card (Tailwind CSS)
+// Create an object and update its data on click of a button and display data in UI.
+// Create employee data in a data.js file and include following fields (name, age, DOB, salary, designation, image, department). Display name, image, designation in UI. Whenever user hover over any name, display complete employee data in alert/modal.
+// Create an array of grocery data, send data from 1 component to another and display it.
+// Create an object pass data from 1 class based to another class based data and display it (Hint: use this.props).
